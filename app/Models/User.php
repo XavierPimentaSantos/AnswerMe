550f67b2caa7cc15f1a66d4603502dfdbfee11f5
Diogo Martins <up202108883@up.pt>
2023-11-17 18:02:56 +0000

    Add User and Settings models

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,58 +2,36 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-// Added to define Eloquent relationships.
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    // Don't add create and update timestamps in database.
-    public $timestamps  = false;
+    public $timestamps = false;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
+        'fullname',
+        'username',
+        'user_password',
         'email',
-        'password',
+        'bio',
+        'birth_date',
+        'nationality',
+        'user_type',
+        'user_settings'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+        'user_password'
+    ]
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
-
-    /**
-     * Get the cards for a user.
-     */
-    public function cards(): HasMany
+    public function settings()
     {
-        return $this->hasMany(Card::class);
+        return $this->belongsTo(Settings::class, 'user_settings');
     }
 }
