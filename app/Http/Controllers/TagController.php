279af3c71444123985a91398782551add29aaaef
Xavier <up202108894@up.pt>
2023-11-23 22:50:14 +0000

    added functions for creating and deleting tags

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Question;
+use App\Models\User;
+
+class TagController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:30',
+        ]);
+
+        $tag = Tag::create([
+            'name' => $request->input('name'),
+        ]);
+
+        return redirect()->route('questions.index') // se calhar enviar para outro sítio
+            ->with('success', 'Tag was added successfully.');
+    }
+
+    public function delete(Request $request, $tag_id)
+    {
+        $tag = Tag::findOrFail($tag_id);
+
+        if(Auth::user()->type!=3) { // assumindo que o código dos admins é 3
+            abort(403, 'Unauthorized'); // apesar de só os admins conseguirem invocar esta função, é melhor verificar se é mesmo um admin a fazê-lo
+        }
+
+        $tag->delete();
+
+        return redirect()->route('questions.index') // se calhar enviar para outro sítio
+            ->with('success', 'Tag was deleted successfully.');
+    }
+}
