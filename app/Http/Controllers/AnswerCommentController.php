3143e4b98f1d45935be86cc2150cf361ffcbd23b
Xavier <up202108894@up.pt>
2023-12-09 12:41:52 +0000

    i have no clue what i was doing

--- app/Http/Controllers/AnswerCommentController.php
+++ app/Http/Controllers/AnswerCommentController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class AnswerCommentController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        
+    }
 }
--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Question;
+use App\Models\AnswerComment;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Answer extends Model
 {
@@ -40,5 +45,10 @@ class Answer extends Model
     public function downvoters() : BelongsToMany
     {
         return $this->belongsToMany(User::class, 'answer_down_votes', 'answer_id', 'user_id');
+    }
+
+    public function comments() : HasMany
+    {
+        return $this->hasMany(AnswerComment::class);
     }
 }
