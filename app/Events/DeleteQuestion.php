917d292d23e732199ce6c90ee2f46ab1fda9775d
Wallen Ribeiro <up202109260@g.uporto.pt>
2023-12-21 05:38:25 +0000

    added AnswerQuestion

---
+++ app/Events/DeleteQuestion.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteQuestion implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $author_id;
+    public $question_title;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($author_id, $question_title)
+    {
+        $this->author_id = $author_id;
+        $this->question_title = $question_title;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn()
+    {
+        return new Channel('user-' . $this->author_id);
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'delete-question';
+    }
+}
